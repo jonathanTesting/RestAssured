ada6bbe3cfbaf77da1e5d298de12b2d39defbfb0
jonathanTesting <tatan0620_512@hotmail.com>
2020-04-29 10:13:51 -0600

    Make metodo2

--- src/test/java/com/api/contender.java
+++ src/test/java/com/api/contender.java
@@ -4,29 +4,28 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 
-
-
 public class contender {
 
-
-
+    public String metodo() {
+        String URL = "https://02p-elevaapi-d01.azurewebsites.net";
+        return URL;
+    }
 
     public void metodo200(RequestSpecification request,Response response){
+        //Body post Request
+        request.header("Content-Type", "application/json");
 
-    //Body post Request
-    request.header("Content-Type", "application/json");
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body is: " + responseBody);
 
-    String responseBody = response.getBody().asString();
-    System.out.println("Response Body is: " + responseBody);
+        int statusCode = response.getStatusCode();
+        System.out.println("Status codigo is: " + statusCode);
+        Assert.assertEquals(statusCode, 200);
 
-    int statusCode = response.getStatusCode();
-    System.out.println("Status codigo is: " + statusCode);
-    Assert.assertEquals(statusCode, 200);
-
-    //status line verification
-    String statusLine = response.getStatusLine();
-    System.out.println("Status linea es:" + statusLine);
-    Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        //status line verification
+        String statusLine = response.getStatusLine();
+        System.out.println("Status linea es:" + statusLine);
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
 }
 
     public void metodo400(RequestSpecification request,Response response){
@@ -47,10 +46,4 @@ public class contender {
         Assert.assertEquals(statusLine, "HTTP/1.1 400 OK");
     }
 
-
-    public String metodo() {
-        String URL = "https://02p-elevaapi-d01.azurewebsites.net";
-        return URL;
-
-    }
 }
